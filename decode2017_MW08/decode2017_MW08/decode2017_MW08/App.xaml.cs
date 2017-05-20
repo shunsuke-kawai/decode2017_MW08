6a67c7aa9a55a986a67033ca2478274eab603bf7
川合 俊介 <Shunsuke_Kawai@jmas.co.jp>
2017-05-20 14:17:59 +0900

    Create Project

---
+++ decode2017_MW08/decode2017_MW08/decode2017_MW08/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Unity;
+using decode2017_MW08.Views;
+using Xamarin.Forms;
+
+namespace decode2017_MW08
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("NavigationPage/MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<NavigationPage>();
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}

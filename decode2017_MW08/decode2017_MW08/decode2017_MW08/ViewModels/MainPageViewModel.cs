6d9122fe45b3fa77ed1bc8e3475fc390fa5817b7
道田隆広 <GRG-Takahiro_Michida@jmas.co.jp>
2017-05-20 16:23:45 +0900

    CustomIndicator追加

--- decode2017_MW08/decode2017_MW08/decode2017_MW08/ViewModels/MainPageViewModel.cs
+++ decode2017_MW08/decode2017_MW08/decode2017_MW08/ViewModels/MainPageViewModel.cs
@@ -1,36 +1,12 @@
 ﻿using Prism.Mvvm;
-using Prism.Navigation;
 
 namespace decode2017_MW08.ViewModels
 {
-    public class MainPageViewModel : BindableBase, INavigationAware
+    public class MainPageViewModel : BindableBase
     {
-        private string _title = "MainPage";
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
-
         public MainPageViewModel()
         {
 
-        }
-
-        public void OnNavigatedFrom(NavigationParameters parameters)
-        {
-
-        }
-
-        public void OnNavigatingTo(NavigationParameters parameters)
-        {
-
-        }
-
-        public void OnNavigatedTo(NavigationParameters parameters)
-        {
-            if (parameters.ContainsKey("title"))
-                Title = (string)parameters["title"] + " and Prism";
         }
     }
 }

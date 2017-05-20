3712f4576a5cc77685f9002afb4e8981889f9feb
shunsuke-kawai <Shunsuke_Kawai@jmas.co.jp>
2017-05-20 16:55:53 +0900

    XamarinFormsバージョンアップ及びイメージリソースエクステンション追加

---
+++ decode2017_MW08/decode2017_MW08/decode2017_MW08/Extensions/ImageResourceExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace decode2017_MW08.Extensions
+{
+    [ContentProperty("Source")]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (string.IsNullOrEmpty(Source)) { return null; }
+
+            if (Device.RuntimePlatform != Device.Windows)
+            {
+                return ImageSource.FromResource(Source);
+            }
+            else
+            {
+                var assembly = typeof(App).GetTypeInfo().Assembly;
+                return ImageSource.FromResource(Source, assembly);
+            }
+        }
+    }
+}

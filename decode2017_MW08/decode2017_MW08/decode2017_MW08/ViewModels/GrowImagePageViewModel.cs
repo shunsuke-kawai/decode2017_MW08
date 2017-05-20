d3e13baae8e8c6c8fdd68040530bebc12720f239
shunsuke-kawai <Shunsuke_Kawai@jmas.co.jp>
2017-05-20 17:56:37 +0900

    イメージ拡大縮小コントロール追加

--- decode2017_MW08/decode2017_MW08/decode2017_MW08/ViewModels/GrowImagePageViewModel.cs
+++ decode2017_MW08/decode2017_MW08/decode2017_MW08/ViewModels/GrowImagePageViewModel.cs
@@ -8,9 +8,25 @@ namespace decode2017_MW08.ViewModels
 {
     public class GrowImagePageViewModel : BindableBase
     {
+        public string _imgTapMsg;
+        public string ImgTapMsg
+        {
+            get { return _imgTapMsg; }
+            set { SetProperty(ref _imgTapMsg, value); }
+        }
+
         public GrowImagePageViewModel()
         {
 
+        }
+
+        ICommand _imgTapCommand;
+        public ICommand ImgTapCommand =>
+            _imgTapCommand ?? (_imgTapCommand = new Command<string>((imgName) => dispMsgAsync(imgName)));
+
+        private void dispMsgAsync(string imgName)
+        {
+            ImgTapMsg = $"{imgName}がタップされました。";
         }
     }
 }

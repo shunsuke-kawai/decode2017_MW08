bd71789624c0aed8f814b532adcc2e74ca05c3f8
川合俊介 <shunsuke_kawai@jmas.co.jp>
2017-05-22 20:09:14 +0900

    CustomEditor修正

---
+++ decode2017_MW08/decode2017_MW08/decode2017_MW08.iOS/Renderers/CustomEditorRenderer.cs
@@ -0,0 +1,20 @@
+﻿using decode2017_MW08.Controls;
+using decode2017_MW08.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEditor), typeof(CustomEditorRenderer))]
+namespace decode2017_MW08.iOS.Renderers
+{
+    public class CustomEditorRenderer : EditorRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+
+            Control.Layer.BorderWidth = 0.6f;
+            Control.Layer.BorderColor = Color.FromRgb(209, 209, 209).ToCGColor();
+            Control.Layer.CornerRadius = 5;
+        }
+    }
+}

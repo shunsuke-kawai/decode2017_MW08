9eab1343e04132c2497ac814a748f0ecf7d3f0ad
川合俊介 <shunsuke_kawai@jmas.co.jp>
2017-05-22 18:46:43 +0900

    HtmlPath追加

---
+++ decode2017_MW08/decode2017_MW08/decode2017_MW08.UWP/Platforms/HtmlPath_UWP.cs
@@ -0,0 +1,15 @@
+﻿using decode2017_MW08.Interfaces;
+using decode2017_MW08.UWP.Platforms;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(HtmlPath_UWP))]
+namespace decode2017_MW08.UWP.Platforms
+{
+    public class HtmlPath_UWP : IHtmlPath
+    {
+        public string GetHtmlPath()
+        {
+            return "ms-appx-web:///";
+        }
+    }
+}
---
+++ decode2017_MW08/decode2017_MW08/decode2017_MW08.iOS/Platforms/HtmlPath_iOS.cs
@@ -0,0 +1,16 @@
+﻿using Foundation;
+using decode2017_MW08.Interfaces;
+using Xamarin.Forms;
+using decode2017_MW08.iOS.Platforms;
+
+[assembly: Dependency(typeof(HtmlPath_iOS))]
+namespace decode2017_MW08.iOS.Platforms
+{
+    public class HtmlPath_iOS : IHtmlPath
+    {
+        public string GetHtmlPath()
+        {
+            return NSBundle.MainBundle.BundleUrl.ToString();
+        }
+    }
+}

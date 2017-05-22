4aaa0dab91e3bb36d25c765fdde98455e59921a4
川合俊介 <shunsuke_kawai@jmas.co.jp>
2017-05-22 19:52:08 +0900

    ListViewHasNoHighlightPage修正

---
+++ decode2017_MW08/decode2017_MW08/decode2017_MW08.UWP/Effects/ListViewDisableClickAndHoverEffect.cs
@@ -0,0 +1,25 @@
+﻿using decode2017_MW08.UWP.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ResolutionGroupName("Xamarin")]
+[assembly: ExportEffect(typeof(ListViewDisableClickAndHoverEffect), "ListViewDisableClickAndHoverEffect")]
+namespace decode2017_MW08.UWP.Effects
+{
+    public class ListViewDisableClickAndHoverEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            var listView = Control as Windows.UI.Xaml.Controls.ListView;
+
+            if (listView == null) { return; }
+
+            listView.ItemContainerStyle = (Windows.UI.Xaml.Style)App.Current.Resources["DisableClickAndHoverStyle"];
+        }
+
+        protected override void OnDetached()
+        {
+
+        }
+    }
+}
---
+++ decode2017_MW08/decode2017_MW08/decode2017_MW08.iOS/Effects/ListViewDisableScrollEffect.cs
@@ -0,0 +1,24 @@
+﻿using decode2017_MW08.iOS.Effects;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("Xamarin")]
+[assembly: ExportEffect(typeof(ListViewDisableScrollEffect), "ListViewDisableScrollEffect")]
+namespace decode2017_MW08.iOS.Effects
+{
+    public class ListViewDisableScrollEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            var listView = Control as UITableView;
+
+            if (listView != null)
+                listView.ScrollEnabled = false;
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
